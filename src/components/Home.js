27835d6b193cf83503b2a040b45714cdf92790f6
Redwan-Shawkat <redwanshawkat@gmail.com>
2024-03-31 23:23:25 +0600

    red

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,50 @@
 
 import React from "react";
 import "../App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faFacebook,
+  faYoutube /* Add other icons as needed */,
+} from "@fortawesome/free-brands-svg-icons";
+
 
 const Home = () => {
   return (
-    <>
+    <div className='Home'>
       <div className='home-container'>
         <div className='home-picture'></div>
         <div className='home-text'>
           <h4>
             {" "}
-            Name: <span className='home-name'> Illias Ahmed</span>
+            Name: <span className='home-name'> Redwan Shawkat </span>
           </h4>
           <h4>
-            {" "}
-            Email: <span className='home-email'> x@gmail.com </span>
+            Email: <span className='home-email'> redwanshawkat@gmail.com </span>
           </h4>
           <h4>
-            {" "}
-            Contact: <span className='home-contact'> 017- </span>
+            Contact: <span className='home-contact'> 01632108860 </span>
           </h4>
+
+          {/* // -----> Social Links  */}
+          <section className='social-links'>
+            {/* // -----> GitHub  */}
+            <a href='https://github.com/Redwan-Shawkat'>
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+
+            {/* // -----> Facebook  */}
+            <a
+              href='https://www.facebook.com/redwanshawkat/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </section>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import '../App.css';
 
+// -----> Font Awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// -----> Import Solid Icons 
+ import {
+   faC,
+ //  faVScodesquare,
+ } from "@fortawesome/free-solid-svg-icons";
+
+// -----> Import brand icons 
+import {
+  faBootstrap,
+  faNode,
+  // faVScodesquare
+} from "@fortawesome/free-brands-svg-icons";
+
+
+// -----> or
+ import {
+   faHtml5,
+   faCss3Alt,
+   faJs,
+   faReact,
+   faGithub,
+   faFigma,
+ } from "@fortawesome/free-brands-svg-icons";
+
+
+// -----> or 
+//import { faNodeJs, faMongodb } from "@fortawesome/free-regular-svg-icons";
+
+// -----> Importing Image 
+import MongoDB from "../assets/MongoDB.png";
+import VS from "../assets/VS.png";
+
 const Skills = () => {
   return (
     <div className='skills-container'>
-      <table className='skills-table'>
+      {/* <table className='skills-table'>
         <thead> SKills </thead>
         <tbody>
           <tr>
@@ -28,7 +62,38 @@ const Skills = () => {
             <td> Figma, GitHub, VScode, Mongoose</td>
           </tr>
         </tbody>
-      </table>
+      </table> */}
+
+      <p className='language_div'>
+        <span className='language'> Language: </span>
+        <FontAwesomeIcon className='HTML' icon={faHtml5} />
+        <FontAwesomeIcon className='CSS' icon={faCss3Alt} />
+        <FontAwesomeIcon className='JavaScript' icon={faJs} />
+        <FontAwesomeIcon className='C' icon={faC} />
+      </p>
+
+      <p className='frameworks_div'>
+        <span className='framework'> Framework: </span>
+        <FontAwesomeIcon className='BOOTSTRAP' icon={faBootstrap} />
+        <FontAwesomeIcon className='REACT' icon={faReact} />
+        {/* <FontAwesomeIcon className='EXPRESS' icon={faExpress} /> */}
+        <FontAwesomeIcon className='NODEJS' icon={faNode} />
+      </p>
+
+      <p className='database_div'>
+        <span className='database'> Database: </span>
+        <div className='mongodb'> </div>
+        <div className='sql'> </div>
+        {/* <FontAwesomeIcon className='MONGODB' icon={faMongodb} /> */}
+      </p>
+
+      <p className='tools_div'>
+        <span className='tools'> Tools: </span>
+        {/* <FontAwesomeIcon className='VSCODE' icon={faVScodesquare} /> */}
+        <div className='VS'> </div>
+        <FontAwesomeIcon className='GITHUB' icon={faGithub} />
+        <FontAwesomeIcon className='FIGMA' icon={faFigma} />
+      </p>
     </div>
   );
 }
